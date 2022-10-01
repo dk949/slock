971b1ca6c7daba6a9f2f5131866d632d3bc0cc4b
dk949 <56653556+dk949@users.noreply.github.com>
2022-10-01 11:26:02 +0100

    vastly simpliefied makefile

--- config.mk
+++ config.mk
@@ -11,13 +11,13 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
+INCS = -I${X11INC}
 LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -s ${LIBS}
+CFLAGS = -std=c99 -pedantic -Wall -Og -g ${INCS} ${CPPFLAGS}
+LDFLAGS = ${LIBS}
 COMPATSRC = explicit_bzero.c
 
 # On OpenBSD and Darwin remove -lcrypt from LIBS
